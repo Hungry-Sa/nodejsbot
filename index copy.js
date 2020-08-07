3619259df3aa050885641182394bde6d6ef2ff37
Hungry-Sa <dudtjrdl1104@naver.com>
2020-08-07 10:20:24 +0900

    bot upload

---
+++ index copy.js
@@ -0,0 +1,63 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const token = 'NzQwOTMzOTI5NzAzMzA5MzE1.XywOfg.Vr2KLfXmRlsOg61LW5vAo1jcCMk';
+const welcomeChannelName = "🚪입장-및-퇴장🚪";
+const byeChannelName = "🚪입장-및-퇴장🚪";
+const welcomeChannelComment = " 님, **Team Platinum** 서버 에 오신걸 환영합니다!!.";
+const byeChannelComment = "님 다음에 또 오세요~ !!.";
+
+client.on('ready', () => {
+  console.log('켰다.');
+});
+
+client.on("guildMemberAdd", (member) => {
+  const guild = member.guild;
+  const newUser = member.user;
+  const welcomeChannel = guild.channels.find(channel => channel.name == welcomeChannelName);
+
+  welcomeChannel.send(`<@${newUser.id}> ${welcomeChannelComment}\n`);
+
+  member.addRole(guild.roles.find(role => role.name == "【Guest】"));
+});
+
+client.on("guildMemberRemove", (member) => {
+  const guild = member.guild;
+  const deleteUser = member.user;
+  const byeChannel = guild.channels.find(channel => channel.name == byeChannelName);
+
+  byeChannel.send(`<@${deleteUser.id}> ${byeChannelComment}\n`);
+});
+
+client.on('message', (message) => {
+  if(message.author.bot) return;
+
+  if(message.content == '!머신') {
+    return message.reply('머신은 바보');
+  }
+
+  if(message.content.startsWith('!전체공지')) {
+    if(checkPermission(message)) return
+    if(message.member != null) { // 채널에서 공지 쓸 때
+      let contents = message.content.slice('!전체공지'.length);
+      message.member.guild.members.array().forEach(x => {
+        if(x.user.bot) return;
+        x.user.send(`<@${message.author.id}> ${contents}`);
+      });
+  
+      return message.reply('공지를 전송했습니다.');
+    } else {
+      return message.reply('채널에서 실행해주세요.');
+    }
+  }
+});
+
+function checkPermission(message) {
+  if(!message.member.hasPermission("MANAGE_MESSAGES")) {
+    message.channel.send(`<@${message.author.id}> ` + "명령어를 수행할 관리자 권한을 소지하고 있지않습니다.")
+    return true;
+  } else {
+    return false;
+  }
+}
+
+client.login(token);
